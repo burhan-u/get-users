b177f754318314f3d1f4329893c75c83182c3d1c
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 00:45:42 +0100

    refactor: add routes index

--- app/app.js
+++ app/app.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
-const healthcheck = require('express-healthcheck');
 const expressLogger = require('./middleware/expressLogger');
-const usersRoute = require('./routes/users');
-const apiRoute = require('./routes/api');
-const notFound = require('./routes/notFound');
+const routes = require('./routes/index');
 
 const app = express();
 
 app.use(helmet());
 app.use(expressLogger);
-
-app.use('/healthcheck', healthcheck());
-app.use('/users', usersRoute);
-app.use('/api', apiRoute);
-app.use(notFound);
+app.use(routes);
 
 module.exports = app;
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const router = require('express').Router();
 const openapiUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-const router = express.Router();
 const openapiDoc = yaml.load(fs.readFileSync('./get-users-spec.yaml', 'utf8'));
-
 const options = { explorer: false };
 
 router.use('/', openapiUi.serve);
