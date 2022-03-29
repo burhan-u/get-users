6a4b892d819d64f3e01c899aaaac3d9e51ddb46a
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-29 01:42:00 +0100

    feat: add logger

--- app/server.js
+++ app/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const app = require('./app');
+const { logger } = require('./middleware/logger');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  logger.info(`Server started on port ${port}`);
 });
