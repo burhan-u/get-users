0d7529e6f1a40ccc7bbc3eb546e08fed92676fb8
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-29 20:38:22 +0100

    feat: add helmet

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const expressLogger = require('./middleware/expressLogger');
 const usersRoute = require('./routes/users');
 const notFound = require('./routes/notFound');
 
 const app = express();
 
+app.use(helmet());
 app.use(expressLogger);
 
 app.use('/users', usersRoute);
