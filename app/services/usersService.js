3dcc230a43f395fe0a33c485b46a25ccc85f1c5b
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 19:10:35 +0100

    feat: catch and return errors

--- app/services/usersService.js
+++ app/services/usersService.js
@@ -1,7 +1,12 @@
 const getUsers = async (repository) => {
-  const allUsers = await repository.fetchAllUsers();
-  const londonUsers = await repository.fetchLondonUsers();
-  return allUsers.concat(londonUsers);
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const allUsers = await repository.fetchAllUsers();
+    const londonUsers = await repository.fetchLondonUsers();
+    return allUsers.concat(londonUsers);
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = getUsers;
--- test/services/usersService.test.js
+++ test/services/usersService.test.js
@@ -1,6 +1,6 @@
 const getUsers = require('../../app/services/usersService');
 
-describe('usersContoller', () => {
+describe('usersService', () => {
   let mockRepository;
 
   beforeEach(() => {
@@ -13,12 +13,29 @@ describe('usersContoller', () => {
   it('should call fetchAllUsers', async () => {
     await getUsers(mockRepository);
 
-    expect(mockRepository.fetchAllUsers).toBeCalledTimes(1);
+    expect(mockRepository.fetchAllUsers).toHaveBeenCalledTimes(1);
   });
 
   it('should call fetchLondonUsers', async () => {
     await getUsers(mockRepository);
 
-    expect(mockRepository.fetchLondonUsers).toBeCalledTimes(1);
+    expect(mockRepository.fetchLondonUsers).toHaveBeenCalledTimes(1);
+  });
+
+  it('should catch errors thrown by external api', async () => {
+    const mockRepositoryRejected = {
+      fetchAllUsers: jest.fn().mockImplementation(() => {
+        throw new Error('Error received from API');
+      }),
+      fetchLondonUsers: jest.fn().mockResolvedValue([{ b: 'b' }]),
+    };
+
+    expect.assertions(2);
+    try {
+      await getUsers(mockRepositoryRejected);
+    } catch (error) {
+      expect(mockRepositoryRejected.fetchLondonUsers).toHaveBeenCalledTimes(0);
+      expect(error.message).toEqual('Error received from API');
+    }
   });
 });
