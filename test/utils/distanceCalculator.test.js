b94d320088c9574ea41b6daf36f7fdd1f4784f99
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 22:27:14 +0100

    fix: remove async

--- test/utils/distanceCalculator.test.js
+++ test/utils/distanceCalculator.test.js
@@ -1,7 +1,7 @@
 const distanceFromLondon = require('../../app/utils/distanceCalculator');
 
 describe('distanceCalculator', () => {
-  it('should return the distance between london and manchester as being greater than 160 miles', async () => {
+  it('should return the distance between london and manchester as being greater than 160 miles', () => {
     const manchester = {
       latitude: 53.485097069530546,
       longitude: -2.2437203995384487,
@@ -11,7 +11,7 @@ describe('distanceCalculator', () => {
     expect(distance).toBeGreaterThan(160);
   });
 
-  it('should return the distance between london and luton as being less than 30 miles', async () => {
+  it('should return the distance between london and luton as being less than 30 miles', () => {
     const luton = {
       latitude: 51.8783703944877,
       longitude: -0.42038421449594304,
@@ -21,7 +21,7 @@ describe('distanceCalculator', () => {
     expect(distance).toBeLessThan(30);
   });
 
-  it('should return the distance between london and wembley as being less than 10 miles', async () => {
+  it('should return the distance between london and wembley as being less than 10 miles', () => {
     const wembley = {
       latitude: 51.550878614953184,
       longitude: -0.30482730443792905,
