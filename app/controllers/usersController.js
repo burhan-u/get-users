5776dc110d0e0323cf83c5f45f7ed8d3927b4fa6
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 19:38:59 +0100

    fix: filename typo

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,9 +2,6 @@ const fetchUsers = require('../repository/fetchUsers');
 const usersService = require('../services/usersService');
 
 const getUsers = async (req, res) => {
-  /* const users = await usersService(fetchUsers);
-  return res.status(200).json(users); */
-
   try {
     const users = await usersService(fetchUsers);
     return res.status(200).json(users);
