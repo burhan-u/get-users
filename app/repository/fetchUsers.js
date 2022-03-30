8d7fa3fb4363e7e6dc4b92db890b425fa350107c
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 02:15:00 +0100

    refactor: fetchUsers

--- app/repository/fetchUsers.js
+++ app/repository/fetchUsers.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
+const { logger } = require('../utils/logger');
 
 const baseUrl = process.env.USERS_REPOSITORY_URL;
 
-const fetchAllUsers = () => axios.get(`${baseUrl}/users`)
-  .then((response) => response.data)
+const fetchUsers = (endpoint) => axios.get(`${baseUrl}/${endpoint}`)
+  .then((response) => {
+    logger.info('Successfully called external API');
+    return response.data;
+  })
   .catch(() => {
+    logger.error('Error received from external API');
     throw new Error('Error received from API');
   });
 
-const fetchLondonUsers = () => axios.get(`${baseUrl}/city/London/users`)
-  .then((response) => response.data)
-  .catch(() => {
-    throw new Error('Error received from API');
-  });
+const fetchAllUsers = () => fetchUsers('users');
+const fetchLondonUsers = () => fetchUsers('city/London/users');
 
 module.exports = { fetchAllUsers, fetchLondonUsers };
