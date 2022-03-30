b10a42697d657725ae7ec3240a1910484e058f64
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 01:59:54 +0100

    feat: handle server error

--- app/repository/fetchUsers.js
+++ app/repository/fetchUsers.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 const baseUrl = process.env.USERS_REPOSITORY_URL;
 
 const fetchAllUsers = () => axios.get(`${baseUrl}/users`)
-  .then((response) => response.data);
+  .then((response) => response.data)
+  .catch(() => {
+    throw new Error('Error received from API');
+  });
 
 module.exports = fetchAllUsers;
--- test/repository/fetchUsers.test.js
+++ test/repository/fetchUsers.test.js
@@ -4,11 +4,51 @@ const fetchAllUsers = require('../../app/repository/fetchUsers');
 jest.mock('axios');
 
 describe('fetch all users data from external api', () => {
+  const mockResponseAllUsers = [
+    {
+      id: 266,
+      first_name: 'Ancell',
+      last_name: 'Garnsworthy',
+      email: 'agarnsworthy7d@seattletimes.com',
+      ip_address: '67.4.69.137',
+      latitude: 51.6553959,
+      longitude: 0.0572553,
+    },
+    {
+      id: 123,
+      first_name: 'Katee',
+      last_name: 'Gopsall',
+      email: 'kgopsallm1@cam.ac.uk',
+      ip_address: '1.1.1.1',
+      latitude: 5.7204203,
+      longitude: 10.901604,
+    },
+  ];
+
   it('sends a request to the correct url', async () => {
     const url = 'https://bpdts-test-app.herokuapp.com/users';
     axios.get.mockResolvedValueOnce({});
     await fetchAllUsers();
 
     expect(axios.get).toHaveBeenCalledWith(url);
+  });
+
+  it('should return an object with user data', async () => {
+    axios.get.mockResolvedValueOnce({ data: mockResponseAllUsers });
+    const res = await fetchAllUsers();
+
+    expect(res).toEqual(mockResponseAllUsers);
+  });
+
+  it('it should throw an error if the server returns with an error', async () => {
+    const errorResponse = { status: 500 };
+    axios.get.mockRejectedValueOnce(errorResponse);
+
+    expect.assertions(1);
+    try {
+      await fetchAllUsers();
+    } catch (error) {
+      expect(error.message).toEqual('Error received from API');
+    }
   });
 });
