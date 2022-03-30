9faa0abbec6942a9414b442e5ed18e93bd938dd7
burhan-u <46581621+burhan-u@users.noreply.github.com>
2022-03-30 19:35:50 +0100

    feat: return error message from usersController

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,8 +2,15 @@ const fetchUsers = require('../repository/fetchUsers');
 const usersService = require('../services/usersService');
 
 const getUsers = async (req, res) => {
-  const users = await usersService(fetchUsers);
-  return res.status(200).json(users);
+  /* const users = await usersService(fetchUsers);
+  return res.status(200).json(users); */
+
+  try {
+    const users = await usersService(fetchUsers);
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = getUsers;
--- test/controllers/usersContoller.test.js
+++ test/controllers/usersContoller.test.js
@@ -1,4 +1,7 @@
 const getUsers = require('../../app/controllers/usersController');
+const usersService = require('../../app/services/usersService');
+
+jest.mock('../../app/services/usersService');
 
 describe('usersContoller', () => {
   const mockRequest = () => {};
@@ -18,14 +21,27 @@ describe('usersContoller', () => {
   });
 
   it('should return with 200 status code', async () => {
+    usersService.mockResolvedValue([]);
     const response = await getUsers(req, res);
 
     expect(response.status).toBeCalledWith(200);
   });
 
   it('should return an array if there are no users', async () => {
+    usersService.mockResolvedValue([]);
     const response = await getUsers(req, res);
 
     expect(response.json).toBeCalledWith([]);
+  });
+
+  it('should return an error message and status 500 if usersService throws an error', async () => {
+    usersService.mockImplementation(() => {
+      throw new Error('Error received from API');
+    });
+    const errorResponse = { message: 'Error received from API' };
+    const response = await getUsers(req, res);
+
+    expect(response.status).toBeCalledWith(500);
+    expect(response.json).toBeCalledWith(errorResponse);
   });
 });
